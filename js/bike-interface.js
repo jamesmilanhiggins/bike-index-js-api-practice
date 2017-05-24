851c9bdbff793150d3418b7ddeeafa347e4ed032
James Higgins and Nicole Santamaria <james@>
2017-05-24 13:42:16 -0700

    update bikeInfo function and display. add scss for future map.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,16 +1,19 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
-var bikeInfo = function(stolen_location, id) {
-  $('.output').append("<li>" + stolen_location + id + "</li>");
+var bikeInfo = function(stolen_location, title, frame_colors, bikes) {
+  bikes.forEach(function(bike) {
+    $('.output').append("<li>" + bike.stolen_location + " " + bike.title + " " + bike.frame_colors + "</li>");
+  });
 };
 
 $(document).ready(function(){
   $('#bike-info').submit(function(event){
     event.preventDefault();
+    $('.output').text("");
     var city = $('#city').val();
     var brand = $('#brand').val();
-    var color = $('#color').val();
+    var distance = $('#distance').val();
     var newBike = new Bike();
-    newBike.getBike(city, brand, color, bikeInfo);
+    newBike.getBike(city, brand, distance, bikeInfo);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -4,10 +4,10 @@ function Bike() {
 
 }
 
-Bike.prototype.getBike = function(city, brand, color, bikeInfo) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&manufacturer=' + brand + '&color=' + color + '&location=' + city + '&distance=1000&stolenness=proximity')
+Bike.prototype.getBike = function(city, brand, distance, bikeInfo) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&manufacturer=' + brand + '&location=' + city + '&distance=' + distance + '&stolenness=proximity')
   .then(function(response) {
-    bikeInfo(response.bikes[0].stolen_location, response.bikes[0].id);
+    bikeInfo(response.bikes.stolen_location, response.bikes.title, response.bikes.frame_colors, response.bikes);
   })
   .fail(function(error){
     $('.output').text(error.responseJSON.message);
