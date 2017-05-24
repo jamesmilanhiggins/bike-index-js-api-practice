9078caa4c7defc7c37e6c8f37e9545dd036e7bef
James Higgins and Nicole Santamaria <nicole@>
2017-05-24 16:40:25 -0700

    add id method and initial longitude and latitude api request.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,7 +1,13 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
-var bikeInfo = function(stolen_location, title, frame_colors, bikes) {
+var bikeInfo = function(stolen_location, title, frame_colors, id, bikes) {
   bikes.forEach(function(bike) {
+    var id = bike.id;
+    console.log(id)
+    // newBike.bikeId(id, locationInfo);
+    // var locationInfo = function(latitude, longitude){
+    //
+    // }
     $('.output').append("<li>" + bike.stolen_location + " " + bike.title + " " + bike.frame_colors + "</li>");
   });
 };
--- js/bike.js
+++ js/bike.js
@@ -7,11 +7,21 @@ function Bike() {
 Bike.prototype.getBike = function(city, brand, distance, bikeInfo) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&manufacturer=' + brand + '&location=' + city + '&distance=' + distance + '&stolenness=proximity')
   .then(function(response) {
-    bikeInfo(response.bikes.stolen_location, response.bikes.title, response.bikes.frame_colors, response.bikes);
+    bikeInfo(response.bikes.stolen_location, response.bikes.title, response.bikes.frame_colors, response.bikes.id, response.bikes);
   })
   .fail(function(error){
     $('.output').text(error.responseJSON.message);
   });
 };
+
+// Bike.prototype.bikeId = function(id, locationInfo) {
+//   $.get('https://bikeindex.org:443/api/v3/bikes/' + id)
+//   .then(function(id_response){
+//     locationInfo(id_response.bike.stolen_record.latitude, id_response.bike.stolen_record.longitude);
+//   })
+//   .fail(function(error){
+//     $('.output').text(error.responseJSON.message);
+//   });
+// };
 
 exports.bikeModule = Bike;
