bcfbc3fba88fce55f320ff7d32912c5970410fd9
James Higgins and Nicole Santamaria <james@>
2017-05-25 14:30:53 -0700

    show map location based on input location

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,6 +1,6 @@
 var Bike = require('./../js/bike.js').bikeModule;
 var ids = [];
-// console.log(ids2)
+var coordinates = [];
 
 var bikeInfo = function(stolen_location, title, frame_colors, id, bikes) {
   bikes.forEach(function(bike) {
@@ -8,6 +8,10 @@ var bikeInfo = function(stolen_location, title, frame_colors, id, bikes) {
     ids.push(id);
     $('.output').append("<li>" + bike.stolen_location + " " + bike.title + " " + bike.frame_colors + "</li>");
   });
+};
+
+var cityCoordinate = function(cityCoord){
+  coordinates.push(cityCoord);
 };
 
 
@@ -19,10 +23,9 @@ $(document).ready(function(){
     var city = $('#city').val();
     var brand = $('#brand').val();
     var distance = $('#distance').val();
-    newBike.getBike(city, brand, distance, bikeInfo);
+    newBike.getBike(city, brand, distance, bikeInfo, cityCoordinate);
   });
   $('#add-map').click(function(){
-
-      newBike.bikeMap(ids);
+      newBike.bikeMap(ids, coordinates);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -3,29 +3,37 @@ var apiKey = require('./../.env').apiKey;
 function Bike() {
 }
 
-Bike.prototype.getBike = function(city, brand, distance, bikeInfo) {
-  // $.get()
+Bike.prototype.getBike = function(city, brand, distance, bikeInfo, cityCoordinate) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&manufacturer=' + brand + '&location=' + city + '&distance=' + distance + '&stolenness=proximity')
   .then(function(response) {
     bikeInfo(response.bikes.stolen_location, response.bikes.title, response.bikes.frame_colors, response.bikes.id, response.bikes);
-  })
-  .fail(function(error){
-    $('.output').text(error.responseJSON.message);
   });
+  $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city)
+  .then(function(response1){
+    var cityCoord = {lat: response1.results[0].geometry.location.lat, lng: response1.results[0].geometry.location.lng};
+
+    cityCoordinate(cityCoord);
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: cityCoord
+    });
+  });
+  // .fail(function(error){
+  //   $('.output').text(error.responseJSON.message);
+  // });
 };
 
-Bike.prototype.bikeMap = function(ids) {
-
+Bike.prototype.bikeMap = function(ids, coordinates) {
+  var cityCoordinates = coordinates[0];
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: {lat: 45.5206297, lng: -122.6774804}
+    center: cityCoordinates
   });
 
   ids.forEach(function(id){
     $.get('https://bikeindex.org:443/api/v3/bikes/' + id)
     .then(function(id_response){
-      // map { zoom: 14, center: {lat: id_response.bike.stolen_record.latitude, lng: id_response.bike.stolen_record.longitude}; }
-
       new google.maps.Marker({
         position: {lat: id_response.bike.stolen_record.latitude, lng:id_response.bike.stolen_record.longitude},
         map: map,
